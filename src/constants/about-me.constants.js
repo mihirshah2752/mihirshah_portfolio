667e6620f2f639c909c595780ecb2d4c858bc5ce
mihirshah2752 <61102621+mihirshah2752@users.noreply.github.com>
2024-06-18 23:50:39 +0530

    design and text changes

--- src/constants/about-me.constants.js
+++ src/constants/about-me.constants.js
@@ -1,8 +1,8 @@
 // src\constants\about-me.constants.js
 
 export const ABOUT_TITLE = "About Me";
-export const ABOUT_HEADER = "who I'm";
-export const INTRO_TITLE = `I'm Mihir and I'm a Developer`;
+export const ABOUT_HEADER = "who i am";
+export const INTRO_TITLE = `I develop interactive web-apps at Axis Bank`;
 export const INTRO_PARAGRAPH = `Currently, I am working as a full stack software engineer at Axis
 Bank having a role of Lead Full Stack Engineer, Strong engineering
 professional with a Bachelor's degree focused in Information
